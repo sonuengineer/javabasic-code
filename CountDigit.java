32a5948b1a19c2448aadd6b51b28ac9fba23ea9d
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2021-09-27 20:51:10 +0530

    Create CountDigit.java

---
+++ CountDigit.java
@@ -0,0 +1,15 @@
+public class CountDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 45938, count=0;
+		for (int i = n; i>0; i=i/10 ) {
+			count++;
+			
+			
+		}
+		System.out.println("The number has " +count+" digits");
+
+	}
+
+}
